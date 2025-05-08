3704496799915ffa5512ca526039747e8bd29a5c
preszzz <107518358+preszzz@users.noreply.github.com>
2025-05-08 17:58:37 -0400

    test: try run map() at early stage

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 # Main entry point for the application
-from src.training.train import train_model
 from src.hyperparameter.tune import run_hyperparameter_tuning
+from src.feature_engineering.feature_loader import preprocess_features
+from src.utils.loader import load_dataset_splits
+import config
 
+def run_training():
+    ds = load_dataset_splits(dataset_name=config.DATASET_NAME)
+
+    # Preprocess data using Hugging Face `map`
+    processed_datasets = ds.map(
+        preprocess_features,
+        batched=True,
+        batch_size=200,
+        writer_batch_size=200,
+        cache_file_names={
+            "train": "train_processed.cache",
+            "valid": "valid_processed.cache",
+            "test": "test_processed.cache"
+        },
+        remove_columns=['audio']
+    )
+
+    print(processed_datasets)
 
 if __name__ == "__main__":
-    run_hyperparameter_tuning(n_trials=20, study_name="ast_hyperparameter_tuning")
+    run_training()
